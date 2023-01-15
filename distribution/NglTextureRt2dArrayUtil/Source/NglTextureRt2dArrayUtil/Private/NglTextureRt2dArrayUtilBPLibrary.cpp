00536d49c76e81048ab1119af347a6a9d24e347f
nagakagachi <nagakagachi@gmail.com>
2023-01-16 00:52:41 +0900

    TextureRenderTarget2dArrayUtilプラグイン.

---
+++ distribution/NglTextureRt2dArrayUtil/Source/NglTextureRt2dArrayUtil/Private/NglTextureRt2dArrayUtilBPLibrary.cpp
@@ -0,0 +1,131 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NglTextureRt2dArrayUtilBPLibrary.h"
+#include "NglTextureRt2dArrayUtil.h"
+
+
+#include "RenderingThread.h"
+#include "Engine/Engine.h"
+#include "CanvasTypes.h"
+#include "Engine/Canvas.h"
+#include "TextureResource.h"
+#include "SceneUtils.h"
+#include "Logging/MessageLog.h"
+#include "RenderGraphUtils.h"
+#include "RenderUtils.h"
+
+#include "TextureRenderTarget2DArrayResource.h"
+#include "Engine/TextureRenderTarget2DArray.h"
+
+#include "Kismet/KismetRenderingLibrary.h"
+
+
+
+UNglTextureRt2dArrayUtilBPLibrary::UNglTextureRt2dArrayUtilBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------
+// フォーマット型の変換
+// Engine\Source\Runtime\Engine\Classes\Engine\TextureRenderTarget2D.h
+bool GetTargetFormatFromPixelFormat(const EPixelFormat PixelFormat, ETextureRenderTargetFormat& OutRTFormat)
+{
+	switch (PixelFormat)
+	{
+	case PF_G8: OutRTFormat = RTF_R8; return true;
+	case PF_R8G8: OutRTFormat = RTF_RG8; return true;
+	case PF_B8G8R8A8: OutRTFormat = RTF_RGBA8; return true;
+
+	case PF_R16F: OutRTFormat = RTF_R16f; return true;
+	case PF_G16R16F: OutRTFormat = RTF_RG16f; return true;
+	case PF_FloatRGBA: OutRTFormat = RTF_RGBA16f; return true;
+
+	case PF_R32_FLOAT: OutRTFormat = RTF_R32f; return true;
+	case PF_G32R32F: OutRTFormat = RTF_RG32f; return true;
+	case PF_A32B32G32R32F: OutRTFormat = RTF_RGBA32f; return true;
+	case PF_A2B10G10R10: OutRTFormat = RTF_RGB10A2; return true;
+	}
+	return false;
+}
+// ---------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------
+void UNglTextureRt2dArrayUtilBPLibrary::NglDrawMaterialToRenderTargetArraySlice(UObject* WorldContextObject, UMaterialInterface* Material, UTextureRenderTarget2DArray* TextureRt2dArray, int SliceIndex)
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(NglDrawMaterialToRenderTargetArraySlice);
+
+	if (!FApp::CanEverRender())
+	{
+		// Returning early to avoid warnings about missing resources that are expected when CanEverRender is false.
+		return;
+	}
+
+	// GraphBuilderによるマテリアル描画実行のテスト.
+	if (Material && TextureRt2dArray)
+	{
+		if (TextureRt2dArray->Slices <= SliceIndex)
+		{
+			FMessageLog("Blueprint").Warning(FText::FromString("NglDrawMaterialToRenderTargetArraySlice Invalid Slice Index."));
+			return;
+		}
+
+
+		SCOPED_DRAW_EVENTF_GAMETHREAD(NglCopyTextureToRenderTargetArray, *TextureRt2dArray->GetFName().ToString());
+
+		auto* p_rt_array_res = TextureRt2dArray->GameThread_GetRenderTargetResource();
+
+		// フォーマット違いやスケール違い,その他特殊な補正のためにDrawMaterialを使用.
+
+		// ArrayのSliceに対するコピー時にフォーマットやサイズが一致するように一時テクスチャの生成.
+		ETextureRenderTargetFormat temp_tex_format;
+		GetTargetFormatFromPixelFormat(TextureRt2dArray->GetFormat(), temp_tex_format);
+		auto temp_tex_Object = NewObject<UTextureRenderTarget2D>(GetTransientPackage());
+		check(temp_tex_Object);
+		temp_tex_Object->RenderTargetFormat = temp_tex_format;;
+		temp_tex_Object->ClearColor = FLinearColor::Blue;
+		temp_tex_Object->InitAutoFormat(TextureRt2dArray->SizeX, TextureRt2dArray->SizeY);
+
+		FTextureRenderTargetResource* RenderTargetResource = temp_tex_Object->GameThread_GetRenderTargetResource();
+
+		// 一時テクスチャに対してDrawMaterial実行.
+		{
+			UKismetRenderingLibrary::DrawMaterialToRenderTarget(WorldContextObject, temp_tex_Object, Material);
+		}
+
+		// ArrayのSliceに対して一時テクスチャからコピー.
+		ENQUEUE_RENDER_COMMAND(FlushDeferredResourceUpdateCommand)(
+			[p_rt_array_res, RenderTargetResource, SliceIndex](FRHICommandListImmediate& RHICmdList)
+			{
+				FRDGBuilder rdgbuilder(RHICmdList);
+
+				// TargetのSlice指定RDGTexture
+				auto rdg_rt_array_ref = p_rt_array_res->GetRenderTargetTexture(rdgbuilder);
+
+				// Input
+				auto rdg_input_ref = RenderTargetResource->GetRenderTargetTexture(rdgbuilder);
+
+				// 一時テクスチャから指定のSliceへコピー.
+				FRHICopyTextureInfo CopyTextureInfo = {};
+				CopyTextureInfo.NumMips = 1;
+				CopyTextureInfo.Size = FIntVector(rdg_input_ref->Desc.GetSize().X, rdg_input_ref->Desc.GetSize().Y, 1);
+				CopyTextureInfo.DestSliceIndex = SliceIndex;
+				AddCopyTexturePass(rdgbuilder, rdg_input_ref, rdg_rt_array_ref, CopyTextureInfo);
+
+				rdgbuilder.Execute();
+			});
+
+		// 破棄
+		temp_tex_Object = nullptr;
+	}
+}
+
+
---
+++ distribution/NglTextureRt2dArrayUtil/Source/NglTextureRt2dArrayUtil/Public/NglTextureRt2dArrayUtilBPLibrary.h
@@ -0,0 +1,36 @@
+﻿// Copyright Taro Horikawa, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "NglTextureRt2dArrayUtilBPLibrary.generated.h"
+
+/* 
+*	Function library class.
+*	Each function in it is expected to be static and represents blueprint node that can be called in any blueprint.
+*
+*	When declaring function you can define metadata for the node. Key function specifiers will be BlueprintPure and BlueprintCallable.
+*	BlueprintPure - means the function does not affect the owning object in any way and thus creates a node without Exec pins.
+*	BlueprintCallable - makes a function which can be executed in Blueprints - Thus it has Exec pins.
+*	DisplayName - full name of the node, shown when you mouse over the node and in the blueprint drop down menu.
+*				Its lets you name the node using characters not allowed in C++ function names.
+*	CompactNodeTitle - the word(s) that appear on the node.
+*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu. 
+*				Good example is "Print String" node which you can find also by using keyword "log".
+*	Category -	the category your node will be under in the Blueprint drop-down menu.
+*
+*	For more info on custom blueprint nodes visit documentation:
+*	https://wiki.unrealengine.com/Custom_Blueprint_Node_Creation
+*/
+UCLASS()
+class UNglTextureRt2dArrayUtilBPLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+
+
+	// Performs a DrawMaterial on the specified TextureArray Slice. This method creates a Temporal Texture and executes CopyTexture.
+	// RenderTargetArrayの指定したSliceに対してDrawMaterialToRenderTargetを実行します. 内部処理に一時リソース生成やテクスチャコピーが含まれます.
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Ngl Draw Material To Texture RenderTarget Array Slice", Keywords = "NglTextureRt2dArrayUtil NglDrawMaterialToRenderTargetArraySlice", WorldContext = "WorldContextObject"), Category = "NglTextureRt2dArrayUtil")
+	static void NglDrawMaterialToRenderTargetArraySlice(UObject* WorldContextObject, UMaterialInterface* Material, class UTextureRenderTarget2DArray* TextureRt2dArray, int SliceIndex = 0);
+
+};
